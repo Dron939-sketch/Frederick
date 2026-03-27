8a200a89a35e7f1dcdf8e99498d9a63be65e350c
Dron939-sketch <Dron939@yandex.ru>
2026-03-27 16:30:18 +0300

    Create init.py

---
+++ backend/repositories/init.py
@@ -0,0 +1,5 @@
+from .user_repo import UserRepository
+from .context_repo import ContextRepository
+from .message_repo import MessageRepository
+
+__all__ = ['UserRepository', 'ContextRepository', 'MessageRepository']
