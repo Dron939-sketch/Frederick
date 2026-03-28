47eb93e496d5df913193a440eb52e0a8282443d7
Dron939-sketch <Dron939@yandex.ru>
2026-03-28 03:51:39 +0300

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,9 @@
+# В папке backend (если есть импорты вида from services import ...)
+touch backend/__init__.py
+
+# Все подпапки, из которых вы импортируете:
+touch backend/services/__init__.py
+touch backend/repositories/__init__.py
+touch backend/modes/__init__.py
+touch backend/hypno/__init__.py
+touch backend/confinement/__init__.py
